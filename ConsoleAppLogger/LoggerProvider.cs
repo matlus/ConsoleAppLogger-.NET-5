2bdabfcb2c27d6bf0c728ff3509f9ef3aa7321aa
Shiv Kumar <shiv.kmr003@gmail.com>
2022-01-04 11:38:46 -0500

    Add project files.

---
+++ ConsoleAppLogger/LoggerProvider.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+using Microsoft.Extensions.Logging.EventLog;
+using System;
+
+namespace ConsoleAppLogger
+{
+    internal sealed class LoggerProvider
+    {
+        private readonly ILoggerFactory loggerFactory;
+        private readonly string applicationNamespaceName;
+        private ILogger logger;
+
+        public LoggerProvider(string applicationNamespaceName, Func<IConfiguration> loggingConfigurationSectionDelegate)
+        {
+            this.applicationNamespaceName = applicationNamespaceName;
+
+            loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                .ClearProviders()
+                .AddConfiguration(loggingConfigurationSectionDelegate())
+#if DEBUG
+                .AddDebug()
+#endif
+                .AddConsole()
+                .AddEventSourceLogger()
+                .AddEventLog(new EventLogSettings { LogName = applicationNamespaceName, SourceName = applicationNamespaceName })
+                .AddApplicationInsights("fac3c5d0-e9a5-420a-b854-2d627340f23b", applicationInsightsLoggerOptions =>
+                {
+                    applicationInsightsLoggerOptions.FlushOnDispose = true;
+                    applicationInsightsLoggerOptions.TrackExceptionsAsExceptionTelemetry = true;
+                });
+            });
+        }
+
+        public ILogger CreateLogger()
+        {
+            if (logger == null)
+            {
+                logger = loggerFactory.CreateLogger(applicationNamespaceName);
+            }
+
+            return logger;
+        }
+
+        public ILogger CreateLogger<T>()
+        {
+            return logger = CreateLogger();
+        }
+    }
+}
---
+++ ConsoleAppLogger/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.EventLog;
+using System;
+using System.Diagnostics;
+
+namespace ConsoleAppLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener("TextWriterOutput.log", "myListener"));
+            Trace.TraceInformation("Test message.");
+            // You must close or flush the trace to empty the output buffer.
+            Trace.Flush();
+
+            var configurationProvider = new ConfigurationProvider();
+            LoggerProvider loggerProvider = new LoggerProvider(nameof(ConsoleAppLogger), configurationProvider.GetLoggingSection);
+
+            var logger = loggerProvider.CreateLogger();
+
+            try
+            {
+                throw new Exception("This is the Exception Message");
+                logger.LogDebug(1, "This is a Debug Level Log - Getting item {Id} at {RequestTime}", 40, DateTime.Now);
+                logger.LogInformation(2, "This is an Informational Log - Getting item {Id} at {RequestTime}", 41, DateTime.Now);
+                logger.LogWarning(2, "This is a Warning Log - Getting item {Id} at {RequestTime}", 41, DateTime.Now);
+            }
+            catch (Exception e)
+            {
+                logger.LogInformation(3, "We Should only see this when an Exception occurs and LogLevel == Error");
+                logger.LogError(4, "An Exception was thrown. Exception Type: {ExceptionType} with Message: {ExceptionMessage}", e.GetType().Name, e.Message);
+            }
+        }
+    }
+}
